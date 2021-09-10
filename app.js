732323016b4279ca21eacfd82533ac3cbfd14ec8
Ebenezer Ashong <ebenezer.ashong@amalitech.org>
2021-09-10 09:25:50 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//human real age.
+const myAge = 49; 
+let earlyYears = 2;
+earlyYears *= 10.5;
+//human age minus 2 years.
+let laterYears = myAge - 2;
+// multiply laterYers by 4.
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+//convert human age to dogs age.
+myAgeInDogYears = earlyYears + laterYears;
+// my name in lower case 
+let myName  = 'Ebenezer Ashong'.toLowerCase();
+//my age in dog years
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
+
